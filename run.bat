d6836518cdb9d861af46e3a102a91c7efcd187f0
Huy Nguyen <vhuy.nguyen@sift-ag.com>
2020-02-03 23:18:15 +0700

    Add code

---
+++ run.bat
@@ -0,0 +1,5 @@
+doctotext\doctotext.exe --pdf E:\Resources\read-pdf-file-allteryx\input\8-september-2019-arrival-summary.pdf >> output.txt
+doctotext\doctotext.exe --pdf E:\Resources\read-pdf-file-allteryx\input\april-2018-arrival-summary-20190314.pdf >> output.txt
+doctotext\doctotext.exe --pdf E:\Resources\read-pdf-file-allteryx\input\december-2018-arrival-summary-20190314.pdf >> output.txt
+doctotext\doctotext.exe --pdf E:\Resources\read-pdf-file-allteryx\input\desktop.ini >> output.txt
+doctotext\doctotext.exe --pdf E:\Resources\read-pdf-file-allteryx\input\january-2019-arrival-summary.pdf >> output.txt
